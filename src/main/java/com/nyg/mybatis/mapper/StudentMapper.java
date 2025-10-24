a185966c2298ef5395c2236d446191a0eb52cddb
NAYUJIN-svg <won10ve8953@gmail.com>
2025-10-24 15:57:22 +0900

    1024_edit
    1024 편집

---
+++ src/main/java/com/nyg/mybatis/mapper/StudentMapper.java
@@ -0,0 +1,25 @@
+package com.nyg.mybatis.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface StudentMapper {
+	
+	List<Student> findAll();
+// 	@Select("SELECT * FROM student WHERE id = #{id}")
+//	Student findById(Long id);
+	
+	Student findById(Long id);
+	
+	
+	void insert(Student student);
+	
+	
+	void update(Student student);
+	
+	
+	void delete(Long id);
+
+}
